9202ac00e9e19bce7f9507ec1dc8e91fbf79ac4e
friej715 <friej715@newschool.edu>
2019-10-16 20:03:26 -0400

    project

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CameraControls : MonoBehaviour
+{
+    public GameObject player;
+    private Camera myCam;
+    private VehicleControls playerControls;
+
+    float minPlayerSpeed = 0;
+    float maxPlayerSpeed = 100;
+
+    float minFOV = 60;
+    float maxFOV = 120;
+
+    // below is a post-processing hint
+    // you do not have to use post-processing or lens distortion if you don't want to!
+    PostProcessVolume volume;
+    LensDistortion lensDistortionLayer;
+    //float minLensDistortion = ???;
+    //float maxLensDistortion = ???;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = GetComponent<Camera>();
+        playerControls = player.GetComponent<VehicleControls>();
+
+        // more post-processing hints
+        volume = GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out lensDistortionLayer);
+        lensDistortionLayer.enabled.Override(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newCameraPosition = player.transform.position - (player.transform.forward * 5);
+        transform.position = newCameraPosition;
+        transform.LookAt(player.transform.position);
+
+        AddFeel();
+    }
+
+    void AddFeel()
+    {
+        float temporaryFOV = Remap(playerControls.currentSpeed, minPlayerSpeed, maxPlayerSpeed, minFOV, maxFOV);
+        float newFOV = Mathf.Clamp(temporaryFOV, minFOV, maxFOV);
+        myCam.fieldOfView = newFOV;
+
+        // how might you adjust the lens distortion based on player speed?
+        // e.g. lensDistortionLayer.intensity.Override(???);
+    }
+
+    float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+}
---
+++ Assets/Scripts/VehicleControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControls : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyHorizontalInput();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyVerticalInput();
+    }
+
+    void ApplyHorizontalInput()
+    {
+        float side = Input.GetAxis("Horizontal");
+        Vector3 rotation = new Vector3(0, side, 0);
+        transform.Rotate(rotation);
+    }
+
+    void ApplyVerticalInput()
+    {
+        float fwd = Input.GetAxis("Vertical");
+        rb.AddForce(transform.forward * fwd);
+        currentSpeed = transform.InverseTransformVector(rb.velocity).z;
+    }
+}
